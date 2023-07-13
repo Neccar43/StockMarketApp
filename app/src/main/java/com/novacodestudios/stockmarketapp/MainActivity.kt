274f795bf1f47a6cac93849100dc706c28bb103e
Abdulkerim Bilgi <akerimbilgi@gmail.com>
2023-07-13 16:08:43 +0300

    Kotlin version updated and destination library bug fixed.

--- app/src/main/java/com/novacodestudios/stockmarketapp/MainActivity.kt
+++ app/src/main/java/com/novacodestudios/stockmarketapp/MainActivity.kt
@@ -10,6 +10,7 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import com.novacodestudios.stockmarketapp.presentation.company_listings.NavGraphs
 import com.novacodestudios.stockmarketapp.ui.theme.StockMarketAppTheme
 import com.ramcosta.composedestinations.DestinationsNavHost
 
@@ -26,7 +27,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    //DestinationsNavHost(navGraph =NavGraps.root )
+                    DestinationsNavHost(navGraph =NavGraphs.root )
                 }
             }
         }
